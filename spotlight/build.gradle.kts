c6ce83dc96ac5723ca6cb40600ac704b62ba03d0
Cyrus Bakhtiari-Haftlang <cyrus.bh@gmail.com>
2023-04-19 16:49:47 +0200

    Gradle related updates

--- spotlight/build.gradle.kts
+++ spotlight/build.gradle.kts
@@ -4,11 +4,24 @@ plugins {
 }
 
 android {
+  namespace = "com.takusemba.spotlight"
+
   compileSdk = 32
   defaultConfig {
     minSdk = 14
   }
+
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+  }
+
+  kotlinOptions {
+    jvmTarget = "1.8"
+  }
 }
+
+
 
 dependencies {
   implementation("org.jetbrains.kotlin:kotlin-stdlib:1.7.22")
